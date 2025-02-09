3aee32399c3801cbd67dcc04d3bcd2f89d3c3401
minjaeleee <dlalswo4602@naver.com>
2025-02-09 16:56:11 +0900

    Solve 00189-easy-awaited

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,7 @@
-type MyAwaited<T> = any
+// Promise로 감싸져 있는 타입일 때는 재귀적으로 순회하며 내부 타입 리턴, 아닐 때는 타입 리턴
+
+type MyAwaited<T> = T extends { then: (onfulfilled: (arg: infer U) => any) => any } 
+  ? MyAwaited<U>
+  : T extends Promise<infer U> 
+    ? MyAwaited<U> 
+    : T;
