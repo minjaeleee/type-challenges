fdd76ce84ca518f7f735fd63828b4793b8f7aa31
minjaeleee <your-email@example.com>
2025-02-25 23:06:08 +0900

    hint

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -1,1 +1,6 @@
-type MyReadonly2<T, K> = any
+// 1. T에 K가 key 값이 존재할 경우 readonly인 케이스 생성
+// 2. & 연산자로, readonly가 붙은 케이스 붙지 않은 케이스 결합
+// 3. T에 K가 key 값이 존재하지 않은 경우, 그대로 키-밸류 생성
+
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+}
