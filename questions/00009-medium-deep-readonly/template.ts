3838e6fe1798928bf37d0648c3cc41c953067780
minjaeleee <your-email@example.com>
2025-04-13 18:30:39 +0900

    Solve 00009-medium-deep-readonly

--- questions/00009-medium-deep-readonly/template.ts
+++ questions/00009-medium-deep-readonly/template.ts
@@ -1,7 +1,12 @@
-// 1. 함수인 케이스
-// 2. 배열인 케이스
-// 3. 기타
+// 1. case1 - 함수
+// 2. case2 - 배열
+// 3. case3 - 객체
 
-type DeepReadonly<T> = {
-  reado
-}
+type DeepReadonly<T> =
+  T extends (...args: any[]) => any
+    ? T
+    : T extends any[]
+      ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+      : T extends object
+        ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+        : T
