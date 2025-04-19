d3c62b85ca8f7dc15c27184d788ad55bb1fd64f3
minjaeleee <your-email@example.com>
2025-04-19 14:21:58 +0900

    Solve 00012-medium-chainable-options

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,8 @@
-type Chainable = {
-  option(key: string, value: any): any
-  get(): any
+type Chainable<R = {}> = {
+  option<K extends string, V>(
+    key: K extends keyof R ? never : K, // 중복 키 제거
+    value: V
+  ): Chainable<R & Record<K, V>>
+  // 최종 객체 생성
+  get(): R
 }
